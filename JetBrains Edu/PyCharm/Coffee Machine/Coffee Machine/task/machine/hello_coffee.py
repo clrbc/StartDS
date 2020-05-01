3eee13829852c05d827440987cd8c553e8c9778d
Clara Correa <clarabc@live.com>
2020-05-01 15:14:42 -0300

    arrumacao

---
+++ JetBrains Edu/PyCharm/Coffee Machine/Coffee Machine/task/machine/hello_coffee.py
@@ -0,0 +1,1 @@
+print('Starting to make a coffee\nGrinding coffee beans\nBoiling water\nMixing boiled water with crushed coffee beans\nPouring coffee into the cup\nPouring some milk into the cup\nCoffee is ready!')
