32025fdd959b827cc30d0223e738ca58a212788c
Clara Correa <clarabc@live.com>
2020-05-01 16:00:15 -0300

    a

--- JetBrains Edu/PyCharm/Coffee Machine/Coffee Machine/task/machine/hello_coffee.py
+++
@@ -1,1 +0,0 @@
-print('Starting to make a coffee\nGrinding coffee beans\nBoiling water\nMixing boiled water with crushed coffee beans\nPouring coffee into the cup\nPouring some milk into the cup\nCoffee is ready!')
--- JetBrains Edu/PyCharm/Coffee Machine/Coffee Machine/task/machine/machines_needs.py
+++
@@ -1,8 +0,0 @@
-cups=input('Write how many cups of coffee you will need:')
-print('For ' + cups + ' cups of coffee you will need:')
-x=(int(cups))
-water=str(x*200)
-milk=str(x*50)
-beans=str(x*15)
-print(water+' ml of water\n'+milk+' ml of milk\n'+beans+' g of coffee beans')
-
